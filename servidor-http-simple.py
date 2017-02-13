de5d635c198950cfde644b53a0a4d4040a89175e
TomasGalindo <tgalindoencinas@yahoo.com>
2017-02-13 11:02:33 +0100

    Servidor simple introduciendo foto y enlace a otra pagina

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """
 Simple HTTP Server
@@ -8,6 +8,7 @@ TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
 """
 
 import socket
+
 
 # Create a TCP objet socket and bind it to a port
 # We bind to 'localhost', therefore only accepts connections from the
@@ -25,12 +26,40 @@ mySocket.listen(5)
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an infinite loop)
 
+
 while True:
-    print 'Waiting for connections'
+    print ('Waiting for connections')
     (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
-                    "\r\n")
+    print ('HTTP request received:')
+    print (recvSocket.recv(1024))
+
+    """ PONER UN TEXTO """
+    recvSocket.send(
+            bytes(
+               "HTTP/1.1 200 OK\r\n\r\n" +
+               "<html><body><h1>Hello World!</h1></body></html>" +
+               "\r\n",
+               "utf-8"
+            )
+    )
+
+    """ PONER EN EL NAVEGADOR UNA IMAGEN DAD UNA URL"""
+#    recvSocket.send(
+#            bytes(
+#               "HTTP/1.1 200 OK\r\n\r\n" +
+#               "<html><body><img src='http://images.khinsider.com/KINGDOM%20HEARTS%202.8/Screenshots/4Gamer%20-%20November%2003%202015/13.jpg'/></body></html>" +
+#               "\r\n",
+#               "utf-8"
+#            )
+#    )
+
+    """ PONER EN EL NAVEGADOR UN ENLACE HACIA OTRA PAGINA"""
+#    recvSocket.send(
+#                    bytes(
+#                        "HTTP/1.1 200 OK\r\n\r\n" +
+#                        "<html><body><a href='http://www.urjc.es'>" +
+#                        "www.urjc.es</a></body></html>",
+#                        "utf-8"
+#                    )
+#    )
     recvSocket.close()
